18da655d89148549dc5ac77478442023ab641059
Prince Sadie <princesadiedev@Princes-MBP.T-mobile.com>
2017-12-15 21:33:32 -0600

    Update video assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #Added by Prince Sadie
+  config.assets.compile = true
+  config.public_file_server.enabled = true
 end
